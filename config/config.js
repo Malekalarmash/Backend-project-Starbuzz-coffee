86c5ca74edd3bfdc3d0ef239fb456641f63e2604
Malek Alarmash <81653238+Malekalarmash@users.noreply.github.com>
2023-03-17 22:54:18 -0400

    deploy test

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+module.exports =
+{
+  "development": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOSTNAME,
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
